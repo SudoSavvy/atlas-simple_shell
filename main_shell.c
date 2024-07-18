f12a40cc30f9ca88df5d34a7b5fbabd2a4b1c520
SudoSavvy <donovanjheape@gmail.com>
2024-07-18 14:45:57 -0500

    Wrote some code into main_shell

--- main_shell.c
+++ main_shell.c
@@ -5,43 +5,44 @@
  * 
  * Return: 0 on success, otherwise an error code.
  */
-
 int main(void)
 {
-    
-char *line = NULL;
-size_t len = 0;
-ssize_t nread;
-char *token;
-char *argv[100];
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    char *token;
+    char *argv[100];
 
-while (1)
-{
-printf(PROMPT)
-nread = getline(&line, &len, stdin);
-if (nread == -1)
-{
-free(line);
-printf("\n");
-break;
-}
+    while (1)
+    {
+        printf(PROMPT); /* Display the prompt to the user */
+        
+        nread = getline(&line, &len, stdin); /* Read a line of input from the user */
+        if (nread == -1) /* Check for end-of-file (Ctrl+D) or error */
+        {
+            free(line); /* Free the allocated memory for the line */
+            printf("\n"); /* Print a newline for formatting */
+            break; /* Exit the loop to terminate the shell */
+        }
 
-int i = 0;
-token = strtok(line, " \n");
-while (token != NULL)
-{
-argv[i++] = token;
-token = strtok(NULL, " \n');")
-}
-argv[i] = NULL;
-if (argv[0] == NULL)
-continue;
+        int i = 0;
+        token = strtok(line, " \n"); /* Split the line into tokens using space and newline as delimiters */
+        while (token != NULL) /* Continue until all tokens are processed */
+        {
+            argv[i++] = token; /* Store each token in the argv array */
+            token = strtok(NULL, " \n"); /* Get the next token */
+        }
+        argv[i] = NULL; /* Null-terminate the argv array */
 
-handle_exit(argv, line);
+        if (argv[0] == NULL) /* If no command is entered, continue to the next iteration */
+            continue;
 
-execute_command(argv);
-}
+        if (strcmp(argv[0], "exit") == 0) /* If the command is "exit" */
+            handle_exit(argv, line); /* Call the exit handler */
 
-free(line);
-return (0);
+        execute_command(argv); /* Execute the entered command */
+    }
+
+    free(line); /* Free the allocated memory for the line before exiting */
+    return (0); /* Return 0 to indicate successful execution */
 }
