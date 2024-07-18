356ce45ce4595dee5c4dbe9a5b20629372df17a0
SudoSavvy <donovanjheape@gmail.com>
2024-07-18 13:24:06 -0500

    Added two files that will be needed eventually

--- main_shell.c
+++ main_shell.c
@@ -1,1 +1,29 @@
+#include "shell.h"
 
+/**
+ * main - Entry point for the shell.
+ * 
+ * Return: 0 on success, otherwise an error code.
+ */
+int main(void)
+{
+char *line = NULL;
+size_t len = 0;
+ssize_t nread;
+char *token;
+char *argv[100];
+
+while (1)
+{
+printf(PROMPT)
+nread = getline(&line, &len, stdin);
+if (nread == -1)
+{
+free(line);
+printf("\n");
+break;
+}
+
+int i = 0;
+}
+}
