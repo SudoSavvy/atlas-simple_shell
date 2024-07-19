765e6c7f6a764c949bdf7f100726e08c88cd7ca9
SudoSavvy <donovanjheape@gmail.com>
2024-07-19 12:26:47 -0500

    Added/Wrote code into execute_command

--- execute_command.c
+++ execute_command.c
@@ -1,1 +1,31 @@
+#include "shell.h"
 
+/**
+ * execute_command - Executes a command
+ * @argv: The argument vector
+ */
+void execute_command(char **argv)
+{
+    pid_t pid;
+
+    pid = fork(); /* Create a new process */
+    if (pid == -1) /* Check if fork failed */
+    {
+        perror("fork"); /* Print error message */
+        exit(EXIT_FAILURE); /* Exit with failure status */
+    }
+    if (pid == 0) /* Child process */
+    {
+        if (execve(argv[0], argv, environ) == -1) /* Execute command */
+        {
+            perror("execve"); /* Print error message */
+            exit(EXIT_FAILURE); /* Exit with failure status */
+        }
+    }
+    else /* Parent process */
+    {
+        int status;
+
+        waitpid(pid, &status, 0); /* Wait for the child process to complete */
+    }
+}
