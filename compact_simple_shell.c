6a48cbb57d39eefb62fa92f7cc26ae9c19188e73
SudoSavvy <donovanjheape@gmail.com>
2024-07-29 19:39:46 -0500

    Trying things to fix execute command and finish tasks

--- compact_simple_shell.c
+++ compact_simple_shell.c
@@ -149,6 +149,10 @@ int file_exists(const char *path)
  * execute_command - Execute external commands.
  * @cmd: The command string input by the user.
  */
+/**
+ * execute_command - Execute external commands.
+ * @cmd: The command string input by the user.
+ */
 void execute_command(const char *cmd)
 {
     char *args[100]; /* Array to hold command and arguments */
@@ -173,12 +177,13 @@ void execute_command(const char *cmd)
         return;
     }
 
-    /* Cast args to const char * const * */
-    execvp(args[0], (const char *const *)args);
+    /* Directly use args as it matches the required type */
+    execvp(args[0], args);
 
     /* If execvp returns, an error occurred */
     perror("execvp");
 }
+
 
 /**
  * main - Main loop of the shell.
